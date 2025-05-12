56f2cd973f1c3302e6954896796bfaeb2ce36736
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-13 04:11:08 +0530

    Create 01_Stars_Rectangle.c

---
+++ C_Programs/02_Day-2/01_Stars_Rectangle.c
@@ -0,0 +1,12 @@
+// A simple program that prtints a grid of asterisks.
+
+#include <stdio.h>
+
+int main() {
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    return 0;
+}
