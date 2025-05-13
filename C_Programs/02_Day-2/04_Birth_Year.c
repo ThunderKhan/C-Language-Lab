5856d548dc896fd2ae559d1a40f0c2c63cab6900
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-13 10:11:55 +0530

    Update 04_Birth_Year.c

--- C_Programs/02_Day-2/04_Birth_Year.c
+++ C_Programs/02_Day-2/04_Birth_Year.c
@@ -1,6 +1,26 @@
+// A basic hard coded program
 #include <stdio.h>
 
 int main() {
     printf("Current Year: %d\nAge: %d\n, Birth Year: %d - %d = %d\n", 2025, 18, 2025, 18, 2025 - 18);
+    return 0;
+}
+
+// A more user-friendly robus program
+
+#include <stdio.h>
+
+int main() {
+    int year;
+    int age;
+
+    printf("Enter the Current Year: ");
+    scanf("%d", &year);
+
+    printf("Enter you Age: ");
+    scanf("%d", &age);
+
+    printf("Your Birth year is: %d\n", year - age);
+
     return 0;
 }
